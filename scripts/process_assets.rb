80da4fd77a6aa431f5b78bdcfefb98cb7c277c5f
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-07-04 07:14:08 -0400

    remove old ffmpeg invocations

--- scripts/process_assets.rb
+++ scripts/process_assets.rb
@@ -42,7 +42,4 @@ Dir.glob(File.join(originals_folder, '*.{mpg,mpeg,mov,avi,mkv,flv,mp4,webm}')).e
     }
     `ffmpeg -i #{file} #{options_string} #{File.join(processed_path, filename)}.#{ext}`
   }
-
-  # `ffmpeg -i #{file} -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k #{File.join(processed_path, filename)}.mp4`
-  # `ffmpeg -i #{file} -codec:v libvpx                               -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -codec:a libvorbis  -b:a 128k #{File.join(processed_path, filename)}.webm`
 }
